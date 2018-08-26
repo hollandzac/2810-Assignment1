02e7419fe2c6fcf9b9d5868694a3b3e7b5ebfb2c
Zachary <holland.zac@gmail.com>
2018-08-27 02:32:11 +1000

    Improve path add for unittests

--- test_word_ladder.py
+++ test_word_ladder.py
@@ -1,16 +1,27 @@
 import unittest
 import word_ladder
+import re
 
 class TestSame(unittest.TestCase):
-    def  test_
 
-class TestFind(unittetst.TestCase):
-    def test_
+    def test_zip_iter(self):
+        testlist = [(c,t) for (c,t) in zip('gold', 'goad')]
+        self.assertEqual([('g','g'),('o','o'),('l','a'),('d','d')], testlist)
+    def test_same(self):
+        same = word_ladder.same('gold','goad')
+        self.assertEqual(3,same)
+        same = word_ladder.same('tree', 'been')
+        self.assertNotEqual(2,same)
+
+# class TestFind(unittetst.TestCase):
+#     def test_
 
 
 class TestBuild(unittest.TestCase):
-    def build_list:
-        assses= tetst
+    def test_search_pattern(self):
+        self.assertNotEqual(True, re.search('.old','gold'))
+        self.assertEqual(None, re.search('.olg','gold'))
+
 
 
 if __name__ == "__main__":
--- word_ladder.py
+++ word_ladder.py
@@ -1,44 +1,65 @@
 #! /usr/bin/env python3
 import re
-#Computes the length of a list made by
+#Computes the length of a list made by the each letter for item that is the same
+#as the target in the same position
 def same(item, target):
- # print([c for (c, t) in zip(item, target) if c == t])
-  return len([c for (c, t) in zip(item, target) if c == t])
+    #using the zip function to iterate over each index in item and target at the
+    #same time if equal add to list then retun length
+    return len([c for (c, t) in zip(item, target) if c == t])
 
 #This function builds a list by searching the words list and if word
 # not in seen or list and the regex search returns true
-def build(pattern, words, seen, list):
+def build(pattern, words, seen, wordlist):
     #the re search '.' character in pattern matches any character except newline
     #will return true as long as all other characters match plus any letter in the
     # '.' postiion
   return [word for word in words
                  if re.search(pattern, word) and word not in seen.keys() and
-                    word not in list]
+                    word not in wordlist]
 
 #finds the path to the target word
 def find(word, words, seen, target, path):
-  list = []
+  wordlist = list()
+  #for each letter in curren word replace it with '.' character and call build function
+  #with that pattern append that list to wordlist
   for i in range(len(word)):
-    list += build(word[:i] + "." + word[i + 1:], words, seen, list)
-    print(list)
-  if len(list) == 0:
-    return False
-  list = sorted([(same(w, target), w) for w in list])
-  for (match, item) in list:
+    wordlist += build(word[:i] + "." + word[i + 1:], words, seen, wordlist)
+    print(wordlist)
+
+  #If no words found to change to remove word from path return false
+  if len(wordlist) == 0:
+      path.pop()
+      print('blue')
+      return False
+#sorts word list into tuples of (number of matches with target word, word) in descending order
+# of  matches
+  wordlist = sorted([(same(w, target), w) for w in wordlist], reverse=True)
+  print(wordlist)
+
+# for each tuple in wordlist if match is 1 off length of word a path has been found
+#add word to path and return true if not add word as key in seen
+  for (match, item) in wordlist:
     if match >= len(target) - 1:
       if match == len(target) - 1:
         path.append(item)
+        #print(path)
       return True
     seen[item] = True
-  for (match, item) in list:
-    path.append(item)
-    if find(item, words, seen, target, path):
-      return True
-    path.pop()
+    print('hi')
+    #print(seen)
+
+
+  for (match, item) in wordlist:
+      if match != 0:
+          path.append(item)
+          print(path)
+          if find(item, words, seen, target, path):
+              return True
+          path.pop()
 
 #Takes dictionary file name as input then opens it
-fname = input("Enter dictionary name: ")#dictionary file name
-file = open(fname)
+#fname = input("Enter dictionary name: ")#dictionary file name
+file = open('dictionary.txt')
 
 lines = file.readlines() #each line in file
 start = input("Enter start word:") #starting word
